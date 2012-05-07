cf503b5d44744b9cf16d2641ab801ca4b6ea3039
William Panting <willtp87@hotmail.com>
2012-05-07 14:20:53 -0300

    added template file

---
+++ islandora_JSON_to_list.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @file
+ *   This file is the template to show list containing JSON data.
+ *
+ * @param array JSON_data
+ */
+
+?>
+<div id="islandora_json_to_list_viewer">
+<?php
+  /**
+   * recursive function that prints an
+   * array object to an unordered list
+   *
+   * @param mixed $JSON_array
+   *   An array to render in a list
+   */
+  function print_JSON_in_list($JSON_array) {
+    print('<UL>');
+    foreach ($JSON_array as $type => $value) :
+      print ('<LI>');
+      if (is_object($value)) :
+        print_JSON_in_list($value);
+      else:
+        print ($type . ':  ' . $value);
+      endif;
+      print ('</LI>');
+    endforeach;
+    print('</UL>');
+  }
+
+  print_JSON_in_list($JSON_datastream);
+?>
+</div>
