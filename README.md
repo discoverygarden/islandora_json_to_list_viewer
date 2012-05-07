2b22c7f220d7780816b521d849506dd4d20c462d
William Panting <willtp87@hotmail.com>
2012-05-07 10:47:21 -0300

    first

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 islandora_json_viewer
 =====================
+
+
+This module is to be used with islandora_viewers.
+It will transform json content into unordered lists for end user display. 
---
+++ islandora_json_to_list_viewer.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ *   This file is the template to show list containing JSON data.
+ *
+ * @param array JSON_data
+ */
+
+?>
+<div id="islandora_json_to_list_viewer">
+</div>
